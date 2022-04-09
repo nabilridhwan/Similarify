6be6e1f590dd0fd7f244176badfd631dabdaeba2
Nabil Ridhwan <nabridhwan@gmail.com>
2022-04-09 20:18:23 +0800

    feat: :sparkles: New favicon and home page changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         dark: "#0f0f0f",
         darkCard: "#171717",
         defaultAlbumCover: "#282828",
+        spotify: "#1DB954",
         brandColor: "#e11d48"
       }
     },
