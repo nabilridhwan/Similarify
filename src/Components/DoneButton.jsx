334ccdde412c9fdc515969d36aff987107516874
Nabil Ridhwan <nabridhwan@gmail.com>
2022-04-10 00:55:15 +0800

    feat: :sparkles: More adjustments to animation of "Done" button

--- src/Components/DoneButton.jsx
+++ src/Components/DoneButton.jsx
@@ -13,20 +13,29 @@ export default function DoneButton({ item, overrideText, k, onClick }) {
 
     const controls = useAnimation();
 
+    console.log("Show")
+
     useEffect(() => {
         (async () => {
             await controls.set({ opacity: 0, y: 70 });
-            await controls.start({ opacity: 1, y: 0, });
+            return await controls.start({ opacity: 1, y: 0, });
         })();
-
     }, [])
 
     useEffect(() => {
         (async () => {
-            const threshold = 3;
+            // const threshold = 3;
             if (item.length > 0) {
-                await controls.set({ rotateX: 0 })
-                await controls.start({ rotateZ: [0, -threshold, 0, threshold, 0], transition: { duration: 0.25, ease: "easeIn" } })
+                const t = 3;
+                await controls.start({
+                    rotateZ: [0, -t, 0, t, 0, -t, 0, t, 0],
+                    scale: [1, 1.15, 1],
+                    transition: {
+                        duration: 0.3,
+                        ease: "easeOut"
+                    }
+                })
+                // controls.start({y: [0, -10, 0]})
             }
         })();
     }, [item])
@@ -35,6 +44,7 @@ export default function DoneButton({ item, overrideText, k, onClick }) {
         <div
             className="w-full flex items-center justify-center">
             <motion.button
+                initial={false}
                 animate={controls}
                 exit={{ opacity: 0, y: 70 }}
                 layout
